08837efc0afcca234441d11deae89ff6f5024024
Pedro <pedralmeida.22.99@gmail.com>
2021-05-26 22:10:41 +0100

    g7 init

---
+++ labs/lab7/CustomDisplayPeriph/CustomDisplayPeriph.gen/sources_1/bd/mb_design/hdl/mb_design_wrapper.vhd
@@ -0,0 +1,58 @@
+--Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
+----------------------------------------------------------------------------------
+--Tool Version: Vivado v.2020.2 (win64) Build 3064766 Wed Nov 18 09:12:45 MST 2020
+--Date        : Wed May 26 15:40:44 2021
+--Host        : DESKTOP-V8LEL7J running 64-bit major release  (build 9200)
+--Command     : generate_target mb_design_wrapper.bd
+--Design      : mb_design_wrapper
+--Purpose     : IP block netlist
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+library UNISIM;
+use UNISIM.VCOMPONENTS.ALL;
+entity mb_design_wrapper is
+  port (
+    an : out STD_LOGIC_VECTOR ( 7 downto 0 );
+    dip_switches_16bits_tri_i : in STD_LOGIC_VECTOR ( 15 downto 0 );
+    dp : out STD_LOGIC;
+    led_16bits_tri_o : out STD_LOGIC_VECTOR ( 15 downto 0 );
+    push_buttons_5bits_tri_i : in STD_LOGIC_VECTOR ( 4 downto 0 );
+    reset : in STD_LOGIC;
+    seg : out STD_LOGIC_VECTOR ( 6 downto 0 );
+    sys_clock : in STD_LOGIC;
+    usb_uart_rxd : in STD_LOGIC;
+    usb_uart_txd : out STD_LOGIC
+  );
+end mb_design_wrapper;
+
+architecture STRUCTURE of mb_design_wrapper is
+  component mb_design is
+  port (
+    sys_clock : in STD_LOGIC;
+    reset : in STD_LOGIC;
+    push_buttons_5bits_tri_i : in STD_LOGIC_VECTOR ( 4 downto 0 );
+    usb_uart_rxd : in STD_LOGIC;
+    usb_uart_txd : out STD_LOGIC;
+    led_16bits_tri_o : out STD_LOGIC_VECTOR ( 15 downto 0 );
+    dip_switches_16bits_tri_i : in STD_LOGIC_VECTOR ( 15 downto 0 );
+    dp : out STD_LOGIC;
+    seg : out STD_LOGIC_VECTOR ( 6 downto 0 );
+    an : out STD_LOGIC_VECTOR ( 7 downto 0 )
+  );
+  end component mb_design;
+begin
+mb_design_i: component mb_design
+     port map (
+      an(7 downto 0) => an(7 downto 0),
+      dip_switches_16bits_tri_i(15 downto 0) => dip_switches_16bits_tri_i(15 downto 0),
+      dp => dp,
+      led_16bits_tri_o(15 downto 0) => led_16bits_tri_o(15 downto 0),
+      push_buttons_5bits_tri_i(4 downto 0) => push_buttons_5bits_tri_i(4 downto 0),
+      reset => reset,
+      seg(6 downto 0) => seg(6 downto 0),
+      sys_clock => sys_clock,
+      usb_uart_rxd => usb_uart_rxd,
+      usb_uart_txd => usb_uart_txd
+    );
+end STRUCTURE;
